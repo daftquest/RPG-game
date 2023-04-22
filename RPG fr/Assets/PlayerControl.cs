3533002bd750d2ca58f8a0cbc6d0ca92e6b208c7
Brian D <bddc2023@gmail.com>
2023-04-22 11:35:48 -0600

    added gravity & ground checking. Changed camera settings
    it took a minute, but now gravity and the ground checking stuff is cool. I also changed the camera to be closer to how i want the final game camera angle to look. I'm going to have to figure out how to change camera angles and stuff, but that's a problem for future me.

--- RPG fr/Assets/PlayerControl.cs
+++ RPG fr/Assets/PlayerControl.cs
@@ -7,6 +7,13 @@ public class PlayerControl : MonoBehaviour
     CharacterController controller;
     Vector2 playerInp;
 
+    Vector3 velocity;
+    float gravity = -20f;
+    public Transform groundChecker;
+    float groundCheckR = 0.1f;
+    public LayerMask groundLayer;
+    bool isGrounded = true;
+
     float playerSpeed = 6f;
     
     // Start is called before the first frame update
@@ -18,8 +25,19 @@ public class PlayerControl : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        isGrounded = Physics.CheckSphere(groundChecker.position, groundCheckR, groundLayer);
+        
+        if (isGrounded && velocity.y < 0)
+        {
+            velocity.y = -3f;
+        }
+
+        velocity.y += gravity * Time.deltaTime;
+
         playerInp.x = Input.GetAxisRaw("Horizontal");
         playerInp.y = Input.GetAxisRaw("Vertical");
+
+        controller.Move(velocity * Time.deltaTime);
 
         Vector3 direction = new Vector3(playerInp.x, 0f, playerInp.y).normalized;
 
