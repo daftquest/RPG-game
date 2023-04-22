cbdae17b4a4b21469c0b5beab27d7841e1f5c0f9
Brian D <bddc2023@gmail.com>
2023-04-22 10:32:52 -0600

    char/cam control, lighting
    reset character and cam control stuff for less control, and changed the lighting a smidge

--- RPG fr/Assets/PlayerControl.cs
+++ RPG fr/Assets/PlayerControl.cs
@@ -4,50 +4,28 @@ using UnityEngine;
 
 public class PlayerControl : MonoBehaviour
 {
-    float speed = 6f;
-    float gravity = -15f;
-    Vector3 velocity;
-    Vector2 pInput;
     CharacterController controller;
-    public Transform cam;
-    float turnSmoothVelocity;
-    public Transform groundChecker;
-    float groundCheckDist = 0.025f;
-    public LayerMask groundLayer;
-    bool isGrounded = true;
-    float turnSmoothTime = 0.1f;
+    Vector2 playerInp;
+
+    float playerSpeed = 6f;
     
     // Start is called before the first frame update
     void Start()
     {
         controller = GetComponent<CharacterController>();
-        Cursor.lockState = CursorLockMode.Locked;
     }
 
     // Update is called once per frame
     void Update()
     {
-        isGrounded = Physics.CheckSphere(groundChecker.position, groundCheckDist, groundLayer);
-        if (isGrounded && velocity.y < 0)
-        {
-            velocity.y = -2f;
-        }
+        playerInp.x = Input.GetAxisRaw("Horizontal");
+        playerInp.y = Input.GetAxisRaw("Vertical");
 
-        pInput.x = Input.GetAxisRaw("Horizontal");
-        pInput.y = Input.GetAxisRaw("Vertical");
-        Vector3 direction = new Vector3(pInput.x, 0f, pInput.y).normalized;
-
-        velocity.y += (gravity * Time.deltaTime);
-        controller.Move(velocity * Time.deltaTime);
+        Vector3 direction = new Vector3(playerInp.x, 0f, playerInp.y).normalized;
 
         if (direction.magnitude >= 0.1f)
         {
-            float targetAngle = Mathf.Atan2(direction.x, direction.z) * Mathf.Rad2Deg + cam.eulerAngles.y;
-            float angle = Mathf.SmoothDampAngle(transform.eulerAngles.y, targetAngle, ref turnSmoothVelocity, turnSmoothTime);
-            transform.rotation = Quaternion.Euler(0f, angle, 0f);
-
-            Vector3 moveDir = Quaternion.Euler(0f, targetAngle, 0f) * Vector3.forward;
-            controller.Move(moveDir.normalized * speed * Time.deltaTime);
+            controller.Move(direction * playerSpeed * Time.deltaTime);
         }
     }
 }
